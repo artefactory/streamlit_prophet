6907fcd7e56d374af85160558f210db4d4ea4a3d
MaximeLutel <maxime.lutel@artefact.com>
2021-05-05 10:45:10 +0200

    fix: remove dialogs

--- streamlit_prophet/cli/__main__.py
+++ streamlit_prophet/cli/__main__.py
@@ -3,14 +3,12 @@
 import typer
 from rich.console import Console
 from streamlit_prophet import __version__
-from streamlit_prophet.cli import dialogs
 
 app = typer.Typer(
     name="streamlit_prophet",
     help="`streamlit_prophet` is a Python cli/package",
     add_completion=True,
 )
-app.add_typer(dialogs.app, name="dialogs")
 console = Console()
 
 
