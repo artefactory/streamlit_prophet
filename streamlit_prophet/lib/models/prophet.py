bf0aabb53a208e81bab3fd42cd2bc0ca8de070e4
MaximeLutel <maxime.lutel@artefact.com>
2021-05-07 12:25:20 +0200

    doc: add docstring for models functions

--- streamlit_prophet/lib/models/prophet.py
+++ streamlit_prophet/lib/models/prophet.py
@@ -1,3 +1,5 @@
+from typing import Tuple
+
 from fbprophet import Prophet
 from fbprophet.diagnostics import cross_validation
 from streamlit_prophet.lib.dataprep.clean import exp_transform
@@ -7,7 +9,21 @@ from streamlit_prophet.lib.utils.logging import suppress_stdout_stderr
 from streamlit_prophet.lib.utils.mapping import convert_into_nb_of_days, convert_into_nb_of_seconds
 
 
-def instantiate_prophet_model(params, use_regressors=True):
+def instantiate_prophet_model(params, use_regressors=True) -> Prophet:
+    """Instantiates a Prophet model with input parameters.
+
+    Parameters
+    ----------
+    params : dict
+        Model parameters.
+    use_regressors : bool
+        Whether or not to add regressors to the model.
+
+    Returns
+    -------
+    Prophet
+        Instantiated (not fitted) Prophet model.
+    """
     seasonality_params = {
         f"{k}_seasonality": params["seasonalities"][k]["prophet_param"]
         for k in {"yearly", "weekly", "daily"}.intersection(set(params["seasonalities"].keys()))
@@ -35,7 +51,37 @@ def forecast_workflow(
     params: dict,
     dates: dict,
     datasets: dict,
-):
+) -> Tuple[dict, dict, dict]:
+    """Trains a Prophet model and makes a prediction on evaluation data and future data if needed.
+
+    Parameters
+    ----------
+    config : dict
+        Lib configuration dictionary, containing information about random seed to use for training.
+    use_cv : bool
+        Whether or not cross-validation is used.
+    make_future_forecast : bool
+        Whether or not to make a forecast on future dates.
+    cleaning : dict
+        Dataset cleaning specifications.
+    resampling : dict
+        Dataset resampling specifications.
+    params : dict
+        Model parameters.
+    dates : dict
+        Dictionary containing all relevant dates for training and forecasting.
+    datasets : dict
+        Dictionary containing all relevant dataframes for training and forecasting.
+
+    Returns
+    -------
+    dict
+        Dictionary containing all relevant dataframes for training and forecasting.
+    dict
+        Dictionary containing fitted Prophet models.
+    dict
+        Dictionary containing the different forecasts.
+    """
     models, forecasts = dict(), dict()
     with suppress_stdout_stderr():
         datasets, models, forecasts = forecast_eval(
@@ -59,7 +105,37 @@ def forecast_eval(
     datasets: dict,
     models: dict,
     forecasts: dict,
-):
+) -> Tuple[dict, dict, dict]:
+    """Trains a Prophet model on training data and makes a prediction on evaluation data.
+
+    Parameters
+    ----------
+    config : dict
+        Lib configuration dictionary, containing information about random seed to use for training.
+    use_cv : bool
+        Whether or not cross-validation is used.
+    resampling : dict
+        Dataset resampling specifications.
+    params : dict
+        Model parameters.
+    dates : dict
+        Dictionary containing all relevant dates for training and forecasting.
+    datasets : dict
+        Dictionary containing all relevant dataframes for training and forecasting.
+    models : dict
+        Dictionary containing instantiated Prophet models.
+    forecasts : dict
+        Dictionary containing the different forecasts.
+
+    Returns
+    -------
+    dict
+        Dictionary containing all relevant datasets for training and forecasting.
+    dict
+        Dictionary containing fitted Prophet models.
+    dict
+        Dictionary containing the different forecasts.
+    """
     models["eval"] = instantiate_prophet_model(params)
     models["eval"].fit(datasets["train"], seed=config["global"]["seed"])
     if use_cv:
@@ -84,7 +160,35 @@ def forecast_future(
     datasets: dict,
     models: dict,
     forecasts: dict,
-):
+) -> Tuple[dict, dict, dict]:
+    """Trains a Prophet model on the whole dataset and makes a prediction on future data.
+
+    Parameters
+    ----------
+    config : dict
+        Lib configuration dictionary, containing information about random seed to use for training.
+    params : dict
+        Model parameters.
+    cleaning : dict
+        Dataset cleaning specifications.
+    dates : dict
+        Dictionary containing all relevant dates for training and forecasting.
+    datasets : dict
+        Dictionary containing all relevant dataframes for training and forecasting.
+    models : dict
+        Dictionary containing instantiated Prophet models.
+    forecasts : dict
+        Dictionary containing the different forecasts.
+
+    Returns
+    -------
+    dict
+        Dictionary containing all relevant datasets for training and forecasting.
+    dict
+        Dictionary containing fitted Prophet models.
+    dict
+        Dictionary containing the different forecasts.
+    """
     models["future"] = instantiate_prophet_model(params, use_regressors=False)
     models["future"].fit(datasets["full"], seed=config["global"]["seed"])
     datasets = make_future_df(dates, datasets, cleaning)
@@ -93,6 +197,20 @@ def forecast_future(
 
 
 def _get_prophet_cv_horizon(dates: dict, resampling: dict) -> str:
+    """Returns cross-validation horizon at the right format for Prophet cross_validation function.
+
+    Parameters
+    ----------
+    dates : dict
+        Dictionary containing cross-validation information.
+    resampling : dict
+        Dictionary containing dataset frequency information.
+
+    Returns
+    -------
+    str
+        Cross-validation horizon at the right format for Prophet cross_validation function.
+    """
     freq = resampling["freq"][-1]
     horizon = dates["folds_horizon"]
     if freq in ["s", "H"]:
