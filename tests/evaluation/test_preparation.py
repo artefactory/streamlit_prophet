fd24328e57cb20378f629516c155252b51bf7d9d
MaximeLutel <maxime.lutel@artefact.com>
2021-05-21 19:48:05 +0200

    fix: update tests for add_time_groupers

--- tests/evaluation/test_preparation.py
+++ tests/evaluation/test_preparation.py
@@ -11,5 +11,5 @@ def test_add_time_groupers(df):
     output = add_time_groupers(df)
     # Output and input dataframes should have the same number of rows
     assert output.shape[0] == df.shape[0]
-    # Output dataframe should have 6 columns more than input dataframe
-    assert output.shape[1] == df.shape[1] + 6
+    # Output dataframe should have 7 columns more than input dataframe
+    assert output.shape[1] == df.shape[1] + 7
