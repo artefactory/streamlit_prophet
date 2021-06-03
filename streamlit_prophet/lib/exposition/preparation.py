c5bc93b0e28a2e2d39f939fb626c70fffae76bbb
julesbertrand <julesbertrand13@gmail.com>
2021-06-03 15:31:12 +0200

    feat: groups school holidays, pubic holidays, and lockdown together for clarity

--- streamlit_prophet/lib/exposition/preparation.py
+++ streamlit_prophet/lib/exposition/preparation.py
@@ -1,9 +1,11 @@
-from typing import Tuple
+from typing import Dict, List, Tuple
 
 import datetime
+from collections import defaultdict
 from datetime import timedelta
 
 import pandas as pd
+from fbprophet import Prophet
 from streamlit_prophet.lib.utils.mapping import convert_into_nb_of_days, convert_into_nb_of_seconds
 
 
@@ -32,7 +34,10 @@ def get_forecast_components(model, forecast_df: pd.DataFrame, include_yhat=False
     for col in components_col_names:
         if col in model.component_modes["multiplicative"]:
             components[col] *= components["trend"]
+
     components = components.set_index("ds")
+    components_mapping = get_components_mapping(components, model, cols_to_drop=["holidays"])
+    components = group_components(components, components_mapping)
     return components
 
 
@@ -58,6 +63,71 @@ def get_forecast_components_col_names(forecast_df: pd.DataFrame) -> list:
         and "additive" not in col
     ]
     return components_col
+
+
+def get_components_mapping(
+    components: pd.DataFrame, model: Prophet, cols_to_drop: List[str] = None
+) -> Dict[str, list]:
+    """Compute a dict with value: list of columns to sum under key: new column name
+    All columns in components will have as key either:
+    - their own name (col: [col]) -> to be left as is
+    - a new name (new_col_name: [col_1, col_2]) -> to be grouped
+    - '_to_drop_' ('_to_drop_': [col_to_drop_1, col_to_drop_2]) -> to be dropped
+
+    Parameters
+    ----------
+    components: pd.DataFrame
+        Forecast output from Prophet model with pre-selected components only
+    model: Prophet
+        Fitted prophet model
+    cols_to_drop: List[str]
+        List of columns to be dropped. Other columns will be either kept or left as is
+
+    Returns
+    -------
+    Dict[str, list]
+        dict with value: list of columns to sum under key: new column name
+    """
+    components_mapping = defaultdict(list)
+    for col in components.columns:
+        if (
+            model.train_holiday_names is not None and col in model.train_holiday_names.values
+        ):  # group
+            if col.startswith("School holiday"):
+                components_mapping["School holidays"].append(col)
+            elif col.startswith("Lockdown"):
+                components_mapping["Lockdown events"].append(col)
+            else:
+                components_mapping["Public holidays"].append(col)
+        elif col in cols_to_drop:  # drop
+            components_mapping["_to_drop_"].append(col)
+        else:
+            components_mapping[col].append(col)  # left as is
+    return components_mapping
+
+
+def group_components(components: pd.DataFrame, components_mapping: Dict[str, list]) -> pd.DataFrame:
+    """Group components based on components_mapping in a copy of the components df
+
+    Parameters
+    ----------
+    components: pd.DataFrame
+        Forecast output from Prophet model with pre-selected components only
+    components_mapping: Dict[str, list]
+        dict with value: list of columns to sum under key: new column name. \
+Can include a '_to_drop_' item to mark columns to be dropped. \
+A column not in any of the mapping values will be dropped.
+
+    Returns
+    -------
+    pd.DataFrame
+        Dataframe with components either left as is, summed or dropped, based on provided mapping
+    """
+    grouped_components = pd.DataFrame(index=components.index)
+    for new_col_name, grouped_cols in components_mapping.items():
+        if new_col_name != "_to_drop_":
+            grouped_components[new_col_name] = components[grouped_cols].sum(axis=1)
+    return grouped_components
 
 
 def get_df_cv_with_hist(forecasts: dict, datasets: dict, models: dict) -> pd.DataFrame:
