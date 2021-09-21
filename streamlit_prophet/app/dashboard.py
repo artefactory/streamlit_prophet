934067422d43159b975d18fbe686693840d3d9c8
MaximeLutel <maxime.lutel@artefact.com>
2021-09-21 09:45:35 +0200

    doc: add source code link

--- streamlit_prophet/app/dashboard.py
+++ streamlit_prophet/app/dashboard.py
@@ -14,7 +14,7 @@ from streamlit_prophet.lib.dataprep.format import (
     resample_df,
 )
 from streamlit_prophet.lib.dataprep.split import get_train_set, get_train_val_sets
-from streamlit_prophet.lib.exposition.export import display_save_experiment_button
+from streamlit_prophet.lib.exposition.export import display_links, display_save_experiment_button
 from streamlit_prophet.lib.exposition.visualize import (
     plot_components,
     plot_future,
@@ -62,6 +62,8 @@ with st.beta_expander("What is this app?", expanded=False):
     st.write("")
 st.write("")
 st.sidebar.image(load_image("logo.png"), use_column_width=True)
+display_links(readme["links"]["repo"], readme["links"]["article"])
+
 
 st.sidebar.title("1. Data")
 
--- streamlit_prophet/lib/exposition/export.py
+++ streamlit_prophet/lib/exposition/export.py
@@ -434,3 +434,24 @@ def display_save_experiment_button(
             dimensions,
         )
         create_save_experiment_button(zip_path)
+
+
+def display_links(repo_link: str, article_link: str) -> None:
+    """Displays an repository and app links.
+
+    Parameters
+    ----------
+    repo_link : str
+        Link of git repository.
+    article_link : str
+        Link of medium article.
+    """
+    col1, col2 = st.sidebar.beta_columns(2)
+    col1.markdown(
+        f"<a style='display: block; text-align: center;' href={repo_link}>Source code</a>",
+        unsafe_allow_html=True,
+    )
+    col2.markdown(
+        f"<a style='display: block; text-align: center;' href={article_link}>App introduction</a>",
+        unsafe_allow_html=True,
+    )
