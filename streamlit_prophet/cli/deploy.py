cd588c6a9748dc915ba152bc23f46d98592a8e65
TomDarmon <darmontom14@gmail.com>
2024-05-06 18:22:17 +0200

    fix: new dockerfile atf deploy

--- streamlit_prophet/cli/deploy.py
+++ streamlit_prophet/cli/deploy.py
@@ -1,6 +1,6 @@
 import typer
 from rich.console import Console
-from streamlit_prophet.app import deploy_streamlit
+from streamlit_prophet.app import deploy_streamlit, deploy_streamlit_with_base_path
 
 app = typer.Typer()
 console = Console()
@@ -10,3 +10,8 @@ console = Console()
 def dashboard() -> None:
     """Deploys the streamlit dashboard."""
     deploy_streamlit()
+
+@app.command()
+def dashboard_with_base_path() -> None:
+    """Deploys the streamlit dashboard with a base path."""
+    deploy_streamlit_with_base_path()
